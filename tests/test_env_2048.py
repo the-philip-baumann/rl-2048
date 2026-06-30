91d0a9112d90841d1ce3e26cdaf5133b92333d89
Philip Baumann <philip.baumann@hispeed.ch>
2026-06-30 22:17:33 +0200

    Add functionality for adding tile at random empty position

---
+++ tests/test_env_2048.py
@@ -0,0 +1,34 @@
+import pytest
+import numpy as np
+
+from env.env_2048 import Env2048
+
+
+@pytest.fixture
+def env():
+    return Env2048()
+
+
+class TestEnv2048:
+        
+    def test_add_tile_at_random_empty_position__correct_value(self, env):
+        expected = 2
+        board = env.board
+
+        env.add_tile_at_random_empty_position(expected)
+
+        x, y = np.where(board == expected)
+        assert len(x) == 1
+        assert len(y) == 1
+        assert board[x[0]][y[0]] == 2
+
+    def test_add_tile_at_random_empty_position__no_overriding(self, env):
+        for i in range(0, env.width * env.height + 1):
+            env.add_tile_at_random_empty_position(i)
+
+        print(env.board)
+        x, y = np.where(env.board == 0)
+        assert len(x) == 0
+        assert len(y) == 0
+
+        
