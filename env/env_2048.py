3cf0e1b2c0d60b66e7ead7982c9dec95ee80fbc5
Philip Baumann <philip.baumann@hispeed.ch>
2026-07-01 00:20:23 +0200

    Implement rotate left

--- env/env_2048.py
+++ env/env_2048.py
@@ -1,5 +1,14 @@
 import numpy as np
 import gymnasium as gym
+import numpy.ma as ma
+
+from enum import Enum
+
+class Actions(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
 
 class Env2048(gym.Env):
     
@@ -14,6 +23,8 @@ class Env2048(gym.Env):
         self.metadata = {"render_modes": ["human"]}
         self.render_mode = render_mode if render_mode in self.metadata["render_modes"] else None
 
+        self.action_space = gym.spaces.Discrete(len(Actions))
+        self.observation_space = gym.spaces.Box(low=0, high=131072, shape=(self.width, self.height), dtype=np.int32) # 2^17 is theoretical highest value
 
     def reset(self): 
         self.board = np.zeros((self.width, self.height), dtype=np.int32)
@@ -32,6 +43,31 @@ class Env2048(gym.Env):
         index = np.random.randint(0, len(coordinates))
         coordinate = coordinates[index]
         self.board[coordinate] = value
+
+    def move_left(self):
+        temp_board = [row[row != 0] for row in self.board]
+
+        for row in temp_board:
+            for i in range(len(row) - 1):
+                if self._is_cell_touching_right_border(row, i):
+                    continue
+                if row[i] == row[i + 1]:
+                    row[i] = row[i] * 2
+                    row[i + 1] = 0
+
+        temp_board = [row[row != 0] for row in temp_board]
+
+        return np.array([np.pad(row, (0, self.width - len(row))) for row in temp_board])
+ 
+
+   
+    def _is_cell_touching_right_border(self,row, i):
+        return i == len(row) - 1
+
+
+
+
+
 
     def render(self):
         if self.render_mode == "human":
--- tests/test_env_2048.py
+++ tests/test_env_2048.py
@@ -6,7 +6,9 @@ from env.env_2048 import Env2048
 
 @pytest.fixture
 def env():
-    return Env2048()
+    env =  Env2048(render_mode="human")
+    env.board = np.zeros((4, 4), dtype=np.int32) 
+    return env
 
 
 class TestEnv2048:
@@ -23,13 +25,84 @@ class TestEnv2048:
         assert board[x[0]][y[0]] == expected
 
     def test_add_tile_at_random_empty_position__no_overriding(self, env):
-        for i in range(0, env.width * env.height + 1 - 2): # -2 since constructor adds two tiles initially
+        for i in range(0, env.width * env.height + 1):
             env.add_tile_at_random_empty_position(i)
 
-        print(env.board)
         x, y = np.where(env.board == 0)
         assert len(x) == 0
         assert len(y) == 0
+
+    def test_move_left__move_value_to_left_boarder(self, env):
+        expected = 2
+        env.board[0][3] = expected
+
+        actual = env.move_left()
+
+        assert actual[0][0] == expected
+
+    def test_move_left__merge_two_values(self, env):
+        expected = 4
+        env.board[0][2] = 2
+        env.board[0][3] = 2
+
+        actual = env.move_left()
+
+        assert actual[0][0] == expected
+
+    def test_move_left__merge_when_seperated_by_a_zero(self, env):
+        expected = 4
+        env.board[0][1] = 2
+        env.board[0][3] = 2
+
+        actual = env.move_left()
+
+        assert actual[0][0] == expected
+
+    def test_move_left__dont_merge_when_different_values(self, env):
+        env.board[0][2] = 2
+        env.board[0][3] = 4
+
+        actual = env.move_left()
+
+        assert actual[0][0] == 2
+        assert actual[0][1] == 4
+
+    def test_move_left__dont_merge_when_seperated_by_a_different_value(self, env):
+        env.board[0][0] = 2
+        env.board[0][1] = 4
+        env.board[0][3] = 2
+
+        actual = env.move_left()
+
+        assert actual[0][0] == 2
+        assert actual[0][1] == 4
+
+    def test_move_left__double_merge(self, env):
+        expected_first_merge = 8
+        expected_second_merge = 4
+        env.board[0][0] = 4
+        env.board[0][1] = 4
+        env.board[0][2] = 2
+        env.board[0][3] = 2
+
+        actual = env.move_left()
+
+        assert actual[0][0] == expected_first_merge 
+        assert actual[0][1] == expected_second_merge
+
+    def test_move_left__merge_after_merge(self, env):
+        expected_merge = 8
+        expected_remaining = 4
+        env.board[0][0] = 4
+        env.board[0][2] = 4
+        env.board[0][3] = 4
+
+        actual = env.move_left()
+
+        assert actual[0][0] == expected_merge
+        assert actual[0][1] == expected_remaining
+
+        
 
 
         
