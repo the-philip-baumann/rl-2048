7651bbc2bcaa0e77e28450aa39f66990bb2c9b58
Philip Baumann <philip.baumann@hispeed.ch>
2026-06-30 22:45:33 +0200

    Fix broken tests due to constructor adding two random tiles

--- tests/test_env_2048.py
+++ tests/test_env_2048.py
@@ -12,7 +12,7 @@ def env():
 class TestEnv2048:
         
     def test_add_tile_at_random_empty_position__correct_value(self, env):
-        expected = 2
+        expected = 20
         board = env.board
 
         env.add_tile_at_random_empty_position(expected)
@@ -20,15 +20,16 @@ class TestEnv2048:
         x, y = np.where(board == expected)
         assert len(x) == 1
         assert len(y) == 1
-        assert board[x[0]][y[0]] == 2
+        assert board[x[0]][y[0]] == expected
 
     def test_add_tile_at_random_empty_position__no_overriding(self, env):
-        for i in range(0, env.width * env.height + 1):
+        for i in range(0, env.width * env.height + 1 - 2): # -2 since constructor adds two tiles initially
             env.add_tile_at_random_empty_position(i)
 
         print(env.board)
         x, y = np.where(env.board == 0)
         assert len(x) == 0
         assert len(y) == 0
+
 
         
